4b729ad1513a30016849711944b0313333ce854a
Kourmanalieva <antoine.kour@gmail.com>
2018-01-24 20:02:21 +0100

    heat map

--- projet.py
+++ projet.py
@@ -29,6 +29,9 @@
 
 
 from tulip import tlp
+from tulip import *
+from tulipogl import *
+from tulipgui import *
 
 import math
 
@@ -83,9 +86,7 @@ def defineGraph(graph, Locus, viewLabel, viewColor, viewSize, Positive, Negative
      viewColor[n] = tlp.Color.Green
 
 ####################################################################################
-# dessin de graph -  
-#utiliser le modÃÂ¨le de forces, justification: modÃÂ¨le qui marche le mieux Ã proiri
-#a adapter selon expression des facteurs pour bonus
+
 
 #################################################################################### 
 def algoGraph(graph, viewLayout):
@@ -96,9 +97,7 @@ def algoGraph(graph, viewLayout):
 
  
 #####################################################################################
- #ÃÂcrire lÃ¢ÂÂalgorithme permettant de construire un graphe complet dont les sommets
 
- # sont les sommets du rÃÂ©seau initial et les poids associÃÂ©s aux arÃÂªtes correspondent
  ##################################################################################### 
 def deleteZero(graph, tp):  
   for m in graph.getNodes() :
@@ -124,7 +123,7 @@ def completeGraph(graph):
         
   
  
-def filtrateEdges(graph, tp):
+def filtrateEdges(graph, tp, viewWeight):
  #selectionner nodes des genes non exprimes
 
  #selectionner edges
@@ -136,9 +135,9 @@ def filtrateEdges(graph, tp):
    for i in tp:
      node1.append(i[source])
      node2.append(i[target])
-     myWeight[n]=pearsonCoeff(node1, node2)
-     if (-0.2>myWeight[n]>0.2):
-       graph.delEdge(n)
+   viewWeight[n]=pearsonCoeff(node1, node2)
+   if (-0.2>viewWeight[n]>0.2):
+     graph.delEdge(n)
      
    
      
@@ -276,14 +275,110 @@ def main(graph):
 
  viewWeight = graph.getDoubleProperty("viewWeight")
  
- tp = [tp1_s,tp10_s,tp11_s,tp12_s,tp13_s,tp14_s,tp15_s,tp16_s,tp17_s,tp2_s,tp13_s,tp4_s,tp5_s,tp6_s,tp7_s,tp8_s,tp9_s]
+ tp = [tp1_s,tp2_s,tp3_s,tp4_s,tp5_s,tp6_s,tp7_s,tp8_s,tp9_s,tp10_s,tp11_s,tp12_s,tp13_s,tp14_s,tp15_s,tp16_s,tp17_s]
  
  defineGraph(graph, Locus, viewLabel, viewColor, viewSize, Positive, Negative)
- algoGraph(graph, viewLayout)
- completeGraph(graph)
+# algoGraph(graph, viewLayout)
+# completeGraph(graph)
  deleteZero(graph, tp)
-# filtrateEdges(graph, tp)
-# 
+# filtrateEdges(graph, tp, viewWeight)
 
 
 
+#heat map
+ listeNodes = []
+ for i in graph.getNodes():
+   listeNodes.append(i)
+   
+ myMap = graph.addSubGraph("map")
+ viewBorderColor = myMap.getColorProperty("viewBorderColor")
+ viewBorderWidth = myMap.getDoubleProperty("viewBorderWidth")
+ viewColor = myMap.getColorProperty("viewColor")
+ viewLayout = myMap.getLayoutProperty("viewLayout")
+ viewShape = myMap.getIntegerProperty("viewShape")
+ viewSize = myMap.getSizeProperty("viewSize")
+ viewShape.setAllNodeValue(4) #des rectangles
+ viewBorderWidth.setAllNodeValue(1.) # avec une bordure d'epaisseur 1
+ viewBorderColor.setAllNodeValue(tlp.Color(0,0,0))	
+ Locus=myMap.getStringProperty("Locus")
+ Expression=myMap.getDoubleProperty("Expression")
+
+ nodes_map={}
+#inverser lignes et colonnes + ajouter couleur
+ for i in range(0, 1105):
+   nodes_map[i]= {}
+   for j in range (0,17):
+     monLocus = Locus[listeNodes[i]]
+     monTp = tp[j]
+     monExpression = monTp[listeNodes[i]]
+     myProperties={}
+     myProperties["Locus"]=monLocus
+     myProperties["Expression"]=monExpression
+     #print(monLocus, " - ", monExpression)
+     nodes_map[i][j]= myMap.addNode(propertiesValues=myProperties)
+ for i in range(0,1105):
+   for j in range(0,17):
+     n = nodes_map[i][j]
+     if(i!=0):
+       myMap.addEdge(nodes_map[i-1][j], n)
+     if(j!=0):
+       myMap.addEdge(nodes_map[i][j-1], n)
+ 
+ params = tlp.getDefaultPluginParameters("FM^3 (OGDF)", myMap)
+ params["Egde Length Property"] = 2
+ success = myMap.applyLayoutAlgorithm('FM^3 (OGDF)', viewLayout, params)
+ 
+ X = -1
+ for ligne in nodes_map:
+	 X += 1
+	 Y = 0
+	 for n in nodes_map[ligne]:
+	  viewLayout[nodes_map[ligne][n]] = tlp.Coord(X*2, Y*2, 0)
+	  Y += 1
+  
+
+
+# Import a grid approximation (with default parameters)
+graph = tlp.importGraph("Grid Approximation")
+
+viewLayout = graph.getLayoutProperty("viewLayout")
+viewSize = graph.getSizeProperty("viewSize")
+viewBorderWidth = graph.getDoubleProperty("viewBorderWidth")
+viewColor = graph.getColorProperty("viewColor")
+viewLabel = graph.getStringProperty("viewLabel")
+
+# Apply an FM^3 layout on it
+fm3pParams = tlp.getDefaultPluginParameters("FM^3 (OGDF)", graph)
+fm3pParams["Unit edge length"] = 100
+graph.applyLayoutAlgorithm("FM^3 (OGDF)", viewLayout, fm3pParams)
+
+# Compute an anonymous degree property
+degree = tlp.DoubleProperty(graph)
+degreeParams = tlp.getDefaultPluginParameters("Degree")
+graph.applyDoubleAlgorithm("Degree", degree, degreeParams)
+
+# Map the node sizes to their degree
+sizeMappingParams = tlp.getDefaultPluginParameters("Metric Mapping", graph)
+sizeMappingParams["property"] = degree
+sizeMappingParams["min size"] = 1
+sizeMappingParams["max size"] = 30
+graph.applySizeAlgorithm("Metric Mapping", viewSize, sizeMappingParams)
+
+# Create a heat map color scale
+heatMap = tlp.ColorScale([tlp.Color(0, 255, 0), tlp.Color(0,0,0), tlp.Color(255, 0, 0)])
+
+# Map the node colors to their degree using the heat map color scale
+# Also set the nodes labels to their id
+for n in graph.getNodes():
+    viewColor[n] = heatMap.getColorAtPos((degree[n] - degree.getNodeMin()) / (degree.getNodeMax() - degree.getNodeMin()))
+    viewLabel[n] = str(n.id)
+
+# Add a border to edges
+viewBorderWidth.setAllEdgeValue(1)
+
+# Create a Node Link Diagram view and set some rendering parameters
+nodeLinkView = tlp.addNodeLinkDiagramView(graph)
+renderingParameters = nodeLinkView.getRenderingParameters()
+renderingParameters.setViewArrow(True)
+renderingParameters.setMinSizeOfLabel(20)
+nodeLinkView.setRenderingParameters(renderingParameters)
